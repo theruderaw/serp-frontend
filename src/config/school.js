034fb6cfaf54dba3d94e75fc645fe59cdef35fc4
theruderaw <theruderaw678@gmail.com>
2026-07-17 23:47:24 +0530

    Added snake_case changes

--- src/config/school.js
+++ src/config/school.js
@@ -1,4 +1,4 @@
 // src/config/school.js
 
 export const SCHOOL_ID =
-    import.meta.env.VITE_SCHOOL_ID || "522e4af4-6e7d-46b4-94cb-66d9cd6af57c";
+    import.meta.env.VITE_SCHOOL_ID || "aa907350-2b62-4525-aae4-7ed0ede024d2";
